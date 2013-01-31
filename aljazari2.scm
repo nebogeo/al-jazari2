d30664ca755d5219017c38ce34b863eb5929d27b
Dave Griffiths <dave@fo.am>
2013-01-31 23:47:04 +0000

    digging works

--- aljazari2.scm
+++ aljazari2.scm
@@ -4,23 +4,26 @@
 (load "blockview.scm")
 
 (define octree
-  (octree-fill-sphere
-   (octree-delete-sphere
-    (octree-fill-sphere
-     (octree-box 
-      (make-empty-octree)        
-      (vector 0 0 0)
-      (vector 64 32 64) 0)
-     (vector 32 32 32) 10 1)
-    (vector 32 35 26) 8)
-   (vector 20 32 12) 5 2))
+  (octree-compress
+   (octree-compress
+    (octree-compress
+     (octree-fill-sphere
+      (octree-delete-sphere
+       (octree-fill-sphere
+        (octree-box 
+         (make-empty-octree)        
+         (vector 0 0 0)
+         (vector 64 32 64) 0)
+        (vector 32 32 32) 10 1)
+       (vector 32 35 26) 8)
+      (vector 20 32 12) 5 2)))))
 
 (define block-view (make-block-view octree))
 ;(destroy-block-view-path block-view (list 0 7))
 
-(define bots (make-bots octree
-                        (list 
-                         (make-bot 0 (vector 20 26 20) '()))))
+(define bots (make-bots 
+              (list 
+               (make-bot 0 (vector 20 26 20) '()))))
 
 (define bot-views (make-bot-views '()))
 
@@ -36,14 +39,7 @@
                            (block-view-update 
                             block-view octree)))
 
-
-               (when (key-pressed "p")
-                     (set! octree 
-                           (octree-delete-sphere 
-                            octree (vector 15 32 12) 6))
-                     (set! block-view 
-                           (block-view-update 
-                            block-view octree)))
-
-               (set! bots (bots-run bots octree))
+               (set! bots (bots-run-code bots octree))
+               (set! octree (bots-run-actions bots octree))
+               (set! bots (bots-clear-actions bots))
                (set! bot-views (bot-views-update bot-views bots))))
--- bots.scm
+++ bots.scm
@@ -3,14 +3,16 @@
 (load "list.scm")
 (load "octree.scm")
 
-(define (make-bot id pos code) (list id pos 0 code))
+(define (make-bot id pos code) (list id pos 0 code 'none))
 (define (bot-id b) (list-ref b 0))
 (define (bot-pos b) (list-ref b 1))
 (define (bot-clock b) (list-ref b 2))
 (define (bot-code b) (list-ref b 3))
+(define (bot-action b) (list-ref b 4))
 (define (bot-modify-pos b v) (list-replace b 1 v))
 (define (bot-modify-clock b v) (list-replace b 2 v))
 (define (bot-modify-code b v) (list-replace b 3 v))
+(define (bot-modify-action b v) (list-replace b 4 v))
 
 (define (bot-test-code bot)
   (let ((dir (vadd
@@ -18,8 +20,12 @@
               (if (key-pressed "s") (vector -1 0 0) (vector 0 0 0))
               (if (key-pressed "a") (vector 0 0 -1) (vector 0 0 0))
               (if (key-pressed "d") (vector 0 0 1) (vector 0 0 0)))))
-    (bot-modify-pos bot (vadd (bot-pos bot) dir)))
-
+    (bot-modify-pos 
+     (if (key-pressed "z") 
+         (bot-modify-action bot 'dig)
+         bot)
+     (vadd (bot-pos bot) dir)))
+  
   #;(bot-modify-pos
    bot
    (vadd 
@@ -31,7 +37,7 @@
      (modulo (bot-clock bot) 4))
     (bot-pos bot))))
 
-(define (bot-run bot octree)
+(define (bot-run-code bot octree)
   (let ((bot (bot-modify-clock bot (+ 1 (bot-clock bot)))))
     ;; check gravity first
     (let ((here (octree-ref octree (bot-pos bot)))
@@ -41,6 +47,11 @@
           (if (eq? under 'e) ;; nothing underneath, go down
               (bot-modify-pos bot (vadd (vector 0 -1 0) (bot-pos bot)))
               (bot-test-code bot)))))) ;; todo: run code
+
+(define (bot-run-action bot octree)
+  (if (eq? (bot-action bot) 'dig)
+      (octree-delete octree (vadd (vector 0 -1 0) (bot-pos bot)))
+      octree))
   
 (define (make-bots l) (list l))
 (define (bots-list bs) (list-ref bs 0)) 
@@ -51,11 +62,25 @@
    bs 
    (cons bot (bots-list bs))))
 
-(define (bots-run bs octree)
+(define (bots-run-code bs octree)
   (bots-modify-list
    bs
    (map
     (lambda (bot)
-      (bot-run bot octree))
+      (bot-run-code bot octree))
     (bots-list bs))))
 
+(define (bots-run-actions bs octree)
+  (foldl
+    (lambda (bot octree)
+      (bot-run-action bot octree))
+    octree
+    (bots-list bs)))
+
+(define (bots-clear-actions bs)
+  (bots-modify-list
+   bs
+   (map
+    (lambda (bot)
+      (bot-modify-action bot 'none))
+    (bots-list bs))))
